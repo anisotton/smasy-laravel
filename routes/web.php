3690d4be80f834c97b171b84bd50f3c2a93bf765
Anderson N. Isotton <anderson@isotton.com.br>
2022-02-03 23:53:55 -0300

    Routes
    - Clean routes

--- routes/web.php
+++ routes/web.php
@@ -7,10 +7,12 @@ use Illuminate\Support\Facades\{
     URL
 };
 use App\Http\Livewire\Smasy\{
-    Settings
+    Settings,
+    User
 };
-
-
+use App\Http\Livewire\Smasy\User\{
+    AccessRules,
+};
 URL::defaults(['language' => app('getClientLanguage')]);
 
 /*
@@ -27,21 +29,27 @@ URL::defaults(['language' => app('getClientLanguage')]);
 Route::redirect('/', App::currentLocale());
 
 Route::group(['prefix' => '{language}',], function () {
-
-    Route::get('/settings/users',[Settings::class,'users'])->name('settings.users');
-
-    Route::get('/settings/createUser',[Settings::class,'new'])->name('settings.createUser');
-
     Route::middleware('auth')->group(function () {
 
+        //Home
         Route::get('/', function () {
             return view('welcome');
         })->name('home');
 
-
+        //Settings
+        Route::group(['prefix' => '/settings',], function () {
+            Route::get('/', function () {
+                return view('welcome');
+            })->name('home');
+            //Users
+            Route::group(['prefix' => '/users',], function () {
+                Route::get('/', [Settings::class, 'users'])->name('user.list');
+                Route::get('/new', [Settings::class, 'new'])->name('user.new');
+                Route::get('/access-rules', AccessRules::class)->name('user.access-rules');
+            });
+        });
     });
 
 
     Auth::routes();
 });
-
